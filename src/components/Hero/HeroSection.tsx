423ef495c2655727acf99cfabd2a229b4db138c1
EddyKelvin <kelvineddyimache@gmail.com>
2023-02-26 23:26:58 +0100

    fix: mobile font size

--- src/components/Hero/HeroSection.tsx
+++ src/components/Hero/HeroSection.tsx
@@ -16,11 +16,11 @@ export default function HeroSection(): JSX.Element {
                         <Text as="h1" color="#fff" fontWeight="300" fontSize="2rem" className="mb-4">
                             Hey, I am
                         </Text>
-                        <Text as="h1" color="#fff" fontWeight="600" fontSize="3rem" marginBottom="2rem">
+                        <Text as="h1" color="#fff" fontWeight="600" fontSize={{ base: "2rem", md: "3rem" }} marginBottom="2rem">
                             George Allen Barnas
                         </Text>
                         <Text as="p" className="typed" fontSize="1.5rem" fontWeight="300">
-                            <Text as="span" className="text-slider-items" fontSize="2rem">
+                            <Text as="span" className="text-slider-items" fontSize="1.5rem">
                                 Am a{" "}
                             </Text>
                             <Box as="span" p="0.4rem" bg="#0081ff69" borderRadius="0.7rem" width="fit-content"><Typed
@@ -28,11 +28,11 @@ export default function HeroSection(): JSX.Element {
                                 typeSpeed={80}
                                 backDelay={1100}
                                 backSpeed={30}
-                                style={{ fontSize: "2rem", fontWeight: 300 }}
+                                style={{ fontSize: "1.5rem", fontWeight: 300 }}
                                 loop
                             /></Box>
                         </Text>
-                        <Text color="white" fontWeight="300" fontSize="2rem">I enjoy building and designing for the web</Text>
+                        <Text color="white" fontWeight="300" fontSize={{ base: "1.5rem", md: "2rem" }}>I enjoy building and designing for the web</Text>
                         {/* <p className="pt-3">
                             <a className="btn btn-primary btn js-scroll px-4" href="#work" role="button">
                                 View My Work
